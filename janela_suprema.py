b6455b066f5b42c13a2f856699e4fbca1e6024b4
Bruno <104274564+Brunin76@users.noreply.github.com>
2023-01-17 21:34:25 -0300

    Add files via upload
    Eu atualmente trabalho em uma produção de janelas de alúmino e esse código serve para simplificar a produção por parte do corte, é so da a altura e a largura que ele ira informar todas as medidas e peças que será utilizada na produção da janela.

---
+++ janela_suprema.py
@@ -0,0 +1,17 @@
+altura=float(input('Entre com a altura: '))
+largura=float(input('Entre com a largura: '))
+posicao=input('Entre com a posição: ')
+
+su007 = altura
+su008 = altura - 3.5
+su039 = altura - 5.0
+su040 = altura - 5.0
+su041 = altura - 5.0
+su102 = altura - 7.4
+print(f'Janela com a Altura  {altura} \nsu007={su007} \nsu008={su008} \nsu039={su039} \nsu040={su040} \nsu041={su041} \nsu102={su102}')
+
+su001 = largura - 2.6
+su002 = largura - 2.6
+su053 = largura/2 - 6.95
+su102 = largura/2 - 7.05
+print(f'A Janela com a largura {largura} \nsu001={su001} \nsu002={su002} \nsu053={su053} \nsu102={su102}')
